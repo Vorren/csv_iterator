d98d77dcbc92ef7075b7bd9f4ac4c8580112e525
Phil Maevskiy <maevskiy@feleven.me>
2014-10-01 16:39:47 +0400

    Added possibility to customize tokenizer behavior by passing an initialized escaped list separator

--- include/csv_iterator.hpp
+++ include/csv_iterator.hpp
@@ -1,3 +1,4 @@
+#ifndef _CSV_ITERATOR_HPP_
 #define _CSV_ITERATOR_HPP_
 
 #include <string>
@@ -15,6 +16,7 @@ namespace csv {
                 bool m_bad;
                 std::istream* m_in;
                 std::string currentLine;
+                boost::escaped_list_separator<char> separator;
                 bool m_currentDirty; // Marks that currentResult is no longer valid
                 Tuple currentResult; // Useful for passing pointers around
             public:
@@ -24,8 +26,8 @@ namespace csv {
                 iterator() : m_bad(true),m_currentDirty(true) {
                 }
 
-                iterator(std::istream& in)
-                    : m_bad(false), m_in(&in),m_currentDirty(true) 
+                iterator(std::istream& in, const boost::escaped_list_separator<char>& separator )
+                    : m_bad(false), m_in(&in),separator(separator), m_currentDirty(true) 
                 {
                     this->operator++();
                 }
@@ -41,8 +43,8 @@ namespace csv {
                     if(m_currentDirty){
                         try {
                             using namespace boost;
-                            typedef tokenizer<escaped_list_separator<char> > Tokens;
-                            Tokens tokens(currentLine);
+                            typedef tokenizer< escaped_list_separator<char> > Tokens;
+                            Tokens tokens(currentLine, separator);
                             details::filler<Tuple>::fill(currentResult,tokens.begin(),tokens.end());
                         } catch (boost::bad_lexical_cast& ex){
                             m_bad = true;
@@ -66,7 +68,7 @@ namespace csv {
                 }
 
                 iterator& operator++() {
-                    bool res = std::getline(*m_in, currentLine);
+                    bool res = (bool)std::getline(*m_in, currentLine);
                     m_bad = !res;
                     m_currentDirty = true;
                     return *this;
